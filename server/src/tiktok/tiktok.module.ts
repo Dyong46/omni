c687a28cd4576a064be9d033cfbaf4cd44456e17
Dylan Vu <96007994+Dyong46@users.noreply.github.com>
2026-03-11 12:42:16 +0700

    style(tiktok): format imports for consistency

--- server/src/tiktok/tiktok.module.ts
+++ server/src/tiktok/tiktok.module.ts
@@ -6,10 +6,7 @@ import { Product } from '../products/entities/product.entity';
 import { OrdersModule } from '../orders/orders.module';
 
 @Module({
-	imports: [
-		TypeOrmModule.forFeature([Product]),
-		OrdersModule,
-	],
+	imports: [TypeOrmModule.forFeature([Product]), OrdersModule],
 	controllers: [TiktokController],
 	providers: [TiktokService],
 })
